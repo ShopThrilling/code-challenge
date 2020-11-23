78001f3bc47666eb74748e50650e00fa57ed6540
Rocio-Infante <shadesofroseblog@gmail.com>
2020-11-23 15:06:59 -0800

    Display a card component with title and description for each article

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,109 @@
 import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import nytimesApi from './data/nytimesApi';
-import ArticleCard from './components/ArticleCard';
+import axios from 'axios';
+
+// //----------------------------- styles ---------------------------------//
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+// //----------------------------- Card for Article ---------------------------------//
 
 const App = () => {
   const [articles, setArticles] = useState([]);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(true);
 
+  const classes = useStyles();
 
   useEffect(() => {
-    getAllArticles();
-  });
+    setLoading(true)
+    fetch(nytimesApi)
+      .then(response => response.json())
+      .then(articleData => {
+        setArticles(articleData)
+        setLoading(false)
+      })
+      .catch(e => {
+        setLoading(false);
+        setError('Fetch failed');
+      });
+  }, []);
 
-  const getAllArticles = async () => {
-    const response = await fetch(nytimesApi);
-    const articleData = await response.json();
-    setArticles(articleData);
-    // console.log(articles.results)
+  if (loading) {
+    return <p>loading...</p>;
   }
+
+  if (error) {
+    return <p>ERROR: {error}</p>;
+  }
+  
   return (
     <div>
-      <ArticleCard />
+    {articles.results.map((article, i) => (
+      <Card className={classes.root} key={i}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {article.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {article.abstract}
+          </Typography>
+          <br />
+        <Typography variant="body2" color="textSecondary" component="p">
+          {article.byline}
+        </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary" href={article.url} target="_blank">
+          Read More
+        </Button>
+      </CardActions>
+    </Card>
+    ))}
     </div>
-  )
-};
+   )
+  }
+
+
+// const App = () => {
+//   // const [articles, setArticles] = useState([]);
+
+//   // useEffect(() => {
+//   //   getAllArticles();
+//   // });
+
+//   // const getAllArticles = async () => {
+//   //   const response = await fetch(nytimesApi);
+//   //   const articleData = await response.json();
+//   //   setArticles(articleData);
+//   //   // console.log(articles.results)
+//   // }
+//   return (
+//     <div>
+//    <ArticleCard /> 
+//     </div>
+//   )
+// };
 
 export default App;
--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,74 +1,80 @@
-import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import nytimesApi from '../data/nytimesApi';
+// import React, { useState, useEffect } from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Card from '@material-ui/core/Card';
+// import CardActionArea from '@material-ui/core/CardActionArea';
+// import CardActions from '@material-ui/core/CardActions';
+// import CardContent from '@material-ui/core/CardContent';
+// import CardMedia from '@material-ui/core/CardMedia';
+// import Button from '@material-ui/core/Button';
+// import Typography from '@material-ui/core/Typography';
+// import nytimesApi from '../data/nytimesApi';
+// import axios from 'axios';
 
-//----------------------------- styles ---------------------------------//
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
-});
+// //----------------------------- styles ---------------------------------//
+// const useStyles = makeStyles({
+//   root: {
+//     maxWidth: 345,
+//   },
+//   media: {
+//     height: 140,
+//   },
+// });
 
-//----------------------------- Card for Article ---------------------------------//
+// //----------------------------- Card for Article ---------------------------------//
 
-export default function ArticleCard() {
-  const [articles, setArticles] = useState([]);
-  const [titles, setTitles] = useState([]);
+// export default function ArticleCard() {
+//   const [data, setData] = useState({ articles: [] });
 
-  const classes = useStyles();
+//   const classes = useStyles();
 
-  useEffect(() => {
-    getAllArticles();
-  });
-
-
-  //------ Fetching all articles from api -------//
-
-  const getAllArticles = async () => {
-    const response = await fetch(nytimesApi);
-    const articleData = await response.json();
-    setArticles(articleData.results);
-    for (let i = 0; i < articles.length; i++){
-      setTitles(articles[i].title);
-    }
-  }
-    
-  return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-         hello
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-  );
-}
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const result = await axios(nytimesApi);
+//       setData(result.data);
+//     };
+//     fetchData();
+//   }, []);
+  
+//   return (
+//     // <div>
+//     // <ul>
+//     //   {data.articles.map(article => (
+//     //     <li key={article.title}>
+//     //       <p>{article.title}</p>
+//     //     </li>
+//     //   ))}
+//     // </ul>
+//     {/* {articleList.articles.map(article => (
+//       <Card className={classes.root}>
+//       <CardActionArea>
+//         <CardMedia
+//           className={classes.media}
+//           image="/static/images/cards/contemplative-reptile.jpg"
+//           title="Contemplative Reptile"
+//         />
+//         <CardContent>
+//           <Typography gutterBottom variant="h5" component="h2">
+//             {article.title}
+//           </Typography>
+//           <Typography variant="body2" color="textSecondary" component="p">
+//             {article.abstract}
+//           </Typography>
+//           <br />
+//         <Typography variant="body2" color="textSecondary" component="p">
+//           {article.byline}
+//         </Typography>
+//         </CardContent>
+//       </CardActionArea>
+//       <CardActions>
+//         <Button size="small" color="primary">
+//           Share
+//         </Button>
+//         <Button size="small" color="primary" target={article.url}>
+//           Learn More
+//         </Button>
+//       </CardActions>
+//     </Card>
+//     ))} */}
+//     {/* </div> */}
+//   );
+// }
