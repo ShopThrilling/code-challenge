b1751f83ce425ce77b445b129a3f7efb240a22bc
Rocio-Infante <shadesofroseblog@gmail.com>
2020-11-23 17:41:00 -0800

    Display cards in multiple rows

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import nytimesApi from './data/nytimesApi';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -7,17 +8,28 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import nytimesApi from './data/nytimesApi';
-import axios from 'axios';
 
 // //----------------------------- styles ---------------------------------//
 const useStyles = makeStyles({
   root: {
-    maxWidth: 345,
+    maxWidth: 330,
+    flexGrow: 1,
+    margin: 10,
+    padding: 0,
+  },
+  cardContainer: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
   },
   media: {
     height: 140,
   },
+  titleText: {
+    textAlign: "center",
+    paddingBottom: 45,
+    padding: 25
+  }
 });
 
 // //----------------------------- Card for Article ---------------------------------//
@@ -50,20 +62,25 @@ const App = () => {
   if (error) {
     return <p>ERROR: {error}</p>;
   }
+
+  // const imageUrls = articles.results.mulimedia.filter(image => image.format === "Normal")
   
   return (
     <div>
+    <h1 className={classes.titleText}>Thrilling Science Articles</h1>
+    <div className={classes.cardContainer}>
     {articles.results.map((article, i) => (
       <Card className={classes.root} key={i}>
       <CardActionArea>
         <CardMedia
           className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
+          // square imageUrl={article.multimedia.url}
+          image="https://static01.nyt.com/images/2020/10/27/science/00VIRUS-REGENERON-HFO1/00VIRUS-REGENERON-HFO1-articleInline-v2.jpg"
+          title="Thumbnail"
         />
         <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {article.title}
+          <Typography gutterBottom variant="h6" component="h2">
+          {article.title}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
             {article.abstract}
@@ -82,28 +99,8 @@ const App = () => {
     </Card>
     ))}
     </div>
+    </div>
    )
   }
-
-
-// const App = () => {
-//   // const [articles, setArticles] = useState([]);
-
-//   // useEffect(() => {
-//   //   getAllArticles();
-//   // });
-
-//   // const getAllArticles = async () => {
-//   //   const response = await fetch(nytimesApi);
-//   //   const articleData = await response.json();
-//   //   setArticles(articleData);
-//   //   // console.log(articles.results)
-//   // }
-//   return (
-//     <div>
-//    <ArticleCard /> 
-//     </div>
-//   )
-// };
 
 export default App;
